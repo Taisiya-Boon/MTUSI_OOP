8eb17d36f85ec836226181c1d0ee3d519100c157
Taisiya-Boon <BoonT-AD@yandex.ru>
2021-01-28 16:27:59 +0300

    lab2-basics-oop добавлена библиотека lombok

--- lab2-basics-oop/src/main/java/ru/mtusi/Point2d.java
+++ lab2-basics-oop/src/main/java/ru/mtusi/Point2d.java
@@ -1,55 +1,15 @@
 package ru.mtusi;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Point2d {
 
     private double xCoord = 0.0d;
-
     private double yCoord = 0.0d;
-
-    public Point2d() {
-
-    }
-
-    public Point2d(double xCoord, double yCoord) {
-        this.xCoord = xCoord;
-        this.yCoord =  yCoord;
-    }
-
-    public double getX() {
-        return xCoord;
-    }
-
-    public double getY() {
-        return yCoord;
-    }
-
-    public void setX(double x) {
-        this.xCoord = x;
-    }
-
-    public void setY(double y) {
-        this.yCoord = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point2d point2d = (Point2d) o;
-        return Double.compare(point2d.xCoord, xCoord) == 0 &&
-                Double.compare(point2d.yCoord, yCoord) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(xCoord, yCoord);
-    }
-
-    @Override
-    public String toString() {
-        return "Point2d(x=" + this.xCoord + ", y=" + this.yCoord + ")";
-    }
 
 }
--- lab2-basics-oop/src/main/java/ru/mtusi/Point3d.java
+++ lab2-basics-oop/src/main/java/ru/mtusi/Point3d.java
@@ -1,72 +1,25 @@
 package ru.mtusi;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Point3d {
 
     private double xCoord = 0.0d;
     private double yCoord = 0.0d;
     private double zCoord = 0.0d;
 
-    public Point3d() {
-    }
-
-    public Point3d(double x, double y, double z) {
-        this.xCoord = x;
-        this.yCoord = y;
-        this.zCoord = z;
-    }
-
-    public double getX() {
-        return this.xCoord;
-    }
-
-    public double getY() {
-        return this.yCoord;
-    }
-
-    public double getZ() {
-        return this.zCoord;
-    }
-
-    public void setX(double x) {
-        this.xCoord = x;
-    }
-
-    public void setY(double y) {
-        this.yCoord = y;
-    }
-
-    public void setZ(double z) {
-        this.zCoord = z;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point3d point3d = (Point3d) o;
-        return Double.compare(point3d.xCoord, xCoord) == 0 &&
-                Double.compare(point3d.yCoord, yCoord) == 0 &&
-                Double.compare(point3d.zCoord, zCoord) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(xCoord, yCoord, zCoord);
-    }
-
-    @Override
-    public String toString() {
-        return "Point3d(x=" + this.xCoord + ", y=" + this.yCoord + ", z=" + this.zCoord + ")";
-    }
-
     public double distanceTo(Point3d point3d) {
-        double distance = Math.sqrt(Math.pow((xCoord - point3d.getX()), 2) +
-                Math.pow((yCoord - point3d.getY()), 2) +
-                Math.pow((zCoord - point3d.getZ()), 2));
+        double distance = Math.sqrt(Math.pow((xCoord - point3d.getXCoord()), 2) +
+                Math.pow((yCoord - point3d.getYCoord()), 2) +
+                Math.pow((zCoord - point3d.getZCoord()), 2));
         return new BigDecimal(distance).setScale(2, RoundingMode.CEILING).doubleValue();
     }
 
